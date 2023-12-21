9ce571db0a0511b3d18142e4969cee1952041cfa
tarde <tarde@A13E13>
2023-12-21 20:54:27 +0100

    Actualizacion de ejercicios 10, 11, y 12

--- 11_autenticacion_usuarios_redireccionamiento/src/main/java/servlets/LoginServlet.java
+++ 11_autenticacion_usuarios_redireccionamiento/src/main/java/servlets/LoginServlet.java
@@ -10,7 +10,7 @@ import service.UsuariosService;
 
 import java.io.IOException;
 
-
+@WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
--- 12_ficha_usuarios/src/main/java/servlets/LoginServlet.java
+++ 12_ficha_usuarios/src/main/java/servlets/LoginServlet.java
@@ -11,7 +11,7 @@ import service.UsuariosService;
 
 import java.io.IOException;
 
-
+@WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
